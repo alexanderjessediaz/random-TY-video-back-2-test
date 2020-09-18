959dac94cb6cc515256d0e7bc695fd0cba77ddbc
AJ <alexanderjessediaz@gmail.com>
2020-09-18 14:42:49 -0600

    remove key info, commented out get requests in index.js

--- index.js
+++ index.js
@@ -14,25 +14,6 @@ require('dotenv').config()
 
 
 
-google.youtube('v3').videos
-
-
-
-google.youtube('v3').search.list({
-    key: process.env.YOUTUBE_TOKEN,
-    part: 'snippet',
-    q:'joji',
-    maxResults: 10
-}).then((response) => {
-    const { data } = response
-    data.items.forEach((item)=> {
-        console.log(`Title: ${item.snippet.title}/nDescription:${item.snippet.description}/n`)
-    })
-    console.log(response.data)
-}).catch((err)=> console.log(err))
-
-
-
 const app = express()
 
 
@@ -45,75 +26,73 @@ app.set('views', __dirname)
 app.use(cookieParser())
 
 
-const OAuth2 = google.auth.OAuth2
 
 
-
-// app.get('/', (req,res) => {
-//     const oauth2client = new OAuth2(
-//         CONFIG.oauth2credentials.client_id,
-//         CONFIG.oauth2credentials.client_secret,
-//         CONFIG.oauth2credentials.redirect_uris[0]
-//         )
+app.get('/', (req,res) => {
+    const oauth2client = new OAuth2(
+        CONFIG.oauth2credentials.client_id,
+        CONFIG.oauth2credentials.client_secret,
+        CONFIG.oauth2credentials.redirect_uris[0]
+        )
         
-//         const loginLink = oauth2client.generateAuthUrl({
-//             access_type: 'offline',
-//             scope: CONFIG.oauth2credentials.scopes
-//         })
+        const loginLink = oauth2client.generateAuthUrl({
+            access_type: 'offline',
+            scope: CONFIG.oauth2credentials.scopes
+        })
         
-//         return res.render('index',{loginLink})
-//     })
+        return res.render('index',{loginLink})
+    })
     
-//     app.get('/oauth2callback', function(req,res){
-//         const oauth2client = new OAuth2(
-//             CONFIG.oauth2credentials.client_id,
-//             CONFIG.oauth2credentials.client_secret,
-//             CONFIG.oauth2credentials.redirect_uris[0]      
-//             )
+    app.get('/oauth2callback', function(req,res){
+        const oauth2client = new OAuth2(
+            CONFIG.oauth2credentials.client_id,
+            CONFIG.oauth2credentials.client_secret,
+            CONFIG.oauth2credentials.redirect_uris[0]      
+            )
             
-//             if (req.query.error){
-//                 return res.redirect('/')
-//             } else {
-//                 oauth2client.getToken(req.query.code,function(err,token){
-//                     if(err) return res.redirect('/')
+            if (req.query.error){
+                return res.redirect('/')
+            } else {
+                oauth2client.getToken(req.query.code,function(err,token){
+                    if(err) return res.redirect('/')
                     
-//                     res.cookie('jwt',jwt.sign(token,CONFIG.JWTsecret))
+                    res.cookie('jwt',jwt.sign(token,CONFIG.JWTsecret))
                     
-//                     return res.redirect('/subscription_list')
-//                 })
-//             }
-//         })
+                    return res.redirect('/subscription_list')
+                })
+            }
+        })
 
       
         
-//         app.get('/subscription_list', function(req,res){
-//             if (!req.cookies.jwt){
-//                 return res.redirect('/')
-//             }
+        app.get('/subscription_list', function(req,res){
+            if (!req.cookies.jwt){
+                return res.redirect('/')
+            }
             
-//             const oauth2client = new OAuth2(
-//                 CONFIG.oauth2credentials.client_id,
-//                 CONFIG.oauth2credentials.client_secret,
-//                 CONFIG.oauth2credentials.redirect_uris[0]      
-//                 )
+            const oauth2client = new OAuth2(
+                CONFIG.oauth2credentials.client_id,
+                CONFIG.oauth2credentials.client_secret,
+                CONFIG.oauth2credentials.redirect_uris[0]      
+                )
                 
-//                 oauth2client.credentials = jwt.verify(req.cookies.jwt, CONFIG.JWTsecret)
+                oauth2client.credentials = jwt.verify(req.cookies.jwt, CONFIG.JWTsecret)
                 
-//                 const service = google.youtube('v3')
+                const service = google.youtube('v3')
                 
                 
-//                 service.subscriptions.list({
-//                     auth:oauth2client,
-//                     mine:true,
-//                     part:'snippet,contentDetails',
-//                     maxResults:25
-//                 })
-//                 .then((response)=> {
-//                     console.log(response)
+                service.subscriptions.list({
+                    auth:oauth2client,
+                    mine:true,
+                    part:'snippet,contentDetails',
+                    maxResults:25
+                })
+                .then((response)=> {
+                    console.log(response)
                     
-//                     return res.render('subscriptions',{subscriptions:response.data.items})
-//                 })
-//             })
+                    return res.render('subscriptions',{subscriptions:response.data.items})
+                })
+            })
             
             
 app.listen(5000, ()=> {
